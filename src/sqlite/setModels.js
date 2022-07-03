798a05df3ff29719780d8301bc98fc50ac18f24c
rotaruteodor <108352567+rotaruteodor@users.noreply.github.com>
2022-07-03 17:47:54 +0300

    Update setModels.js

--- src/sqlite/setModels.js
+++ src/sqlite/setModels.js
@@ -8,11 +8,8 @@ require('./models/bugs');
 require('./models/testers');
 require('./models/members');
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 app.use("/api", require("./routes/usersRoute"));
 app.use("/api", require("./routes/projectsRoute"));
@@ -22,17 +19,11 @@ app.use("/api", require("./routes/membersRoute"));
 app.use("/api", require("./routes/deleteTesterRoute"));
 app.use("/api", require("./routes/deleteMemberRoute"));
 
-
-
-
 app.listen(port, async () => {
     console.log('The server is running on http://localhost:' + port);
     try {
         await sequelize.authenticate();
         console.log('The connection to the database has been initialised');
-        
-        
-
     } catch (error) {
         console.error(error);
     }
